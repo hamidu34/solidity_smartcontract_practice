2f6fd023c95fd33a0b86d837a321f2bafc2fa8f8
hamidu34 <37995827+hamidu34@users.noreply.github.com>
2022-07-05 10:51:03 +0500

    Solidity Ethereum

---
+++ practice_workspace/test.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.4.22 <0.9.0;
+
+contract First{
+        
+    function doyourwork() public pure returns(uint){
+        uint a= 20;
+        uint b= 10;
+        return a+b;
+    }
+}
